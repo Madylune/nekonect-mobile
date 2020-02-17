878efeed8b120000a444e39eaf759d7cad38d2fd
Julie Nginn <nginn.julie@gmail.com>
2020-02-17 09:36:52 +0100

    Add api

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -3,6 +3,26 @@ import { Text } from 'react-native'
 import { ButtonGroup } from 'react-native-elements'
 import styled from 'styled-components'
 import map from 'lodash/fp/map'
+import get from 'lodash/fp/get'
+import { SERVER_URL } from '../api/server'
+
+const io = require('socket.io-client')
+const socket = io(SERVER_URL)
+
+const BUTTONS = [
+  {
+    title: 'Manger',
+    action: 'eat'
+  },
+  {
+    title: 'Jouer',
+    action: 'play'
+  },
+  { 
+    title: 'Dormir',
+    action: 'sleep'
+  }
+]
 
 const Button = ({ text }) => <Text>{text}</Text>
 
@@ -14,16 +34,18 @@ const BottomBar = () => {
   const [selectedIndex, setSelectedIndex] = useState(undefined)
 
   const updateIndex = useCallback(
-    selectedIndex => setSelectedIndex(selectedIndex), [setSelectedIndex]
+    selectedIndex => {
+      setSelectedIndex(selectedIndex), [setSelectedIndex]
+      socket.emit(get([selectedIndex, 'action'], BUTTONS))
+    }
   )
 
-  const buttons = ['Manger', 'Jouer', 'Dormir']
   return (
     <StyledButtonGroup
       onPress={updateIndex}
       selectedIndex={selectedIndex}
       buttons={
-        map(button => <Button text={button}/>, buttons)
+        map(button => <Button text={button.title}/>, BUTTONS)
       }
       containerStyle={{height: 50}} />
   )
